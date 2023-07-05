1f46d8c5b911ed060aca48d87fd592f644f6a54f
eichelb4rt <maurice.wenig@gmail.com>
2023-07-05 20:14:34 +0200

    added support for more mensen

--- like.sh
+++ like.sh
@@ -2,6 +2,8 @@
 
 # sleep time in seconds
 sleep_time=0.5
+# appropriate mensa website of your choice
+mensa_page="https://www.stw-thueringen.de/mensen/jena/mensa-ernst-abbe-platz.html"
 
 # constants for the lynx output
 n_lines_per_options=6
@@ -11,7 +13,7 @@ gebinde_offset=3
 name_offset=5
 
 # read the options from the website
-options=$(lynx -dump "https://www.stw-thueringen.de/mensen/jena/mensa-ernst-abbe-platz.html" -connect_timeout=10 | tail -n +210 | head -n -332 | grep -P '^\s*\d\d\d\d-\d\d-\d\d' -B 2 -A 3)
+options=$(lynx --dump "$mensa_page" -connect_timeout=10 | grep -P '^\s*\d\d\d\d-\d\d-\d\d' -B 2 -A 3)
 n_lines=$(echo "$options" | wc -l)
 # the number of options is the number of lines (+ 1 extra seperator) divided by the number of lines per options (+ the separators)
 n_options=$((("$n_lines" + 1) / ("$n_lines_per_options" + 1)))
--- read_options.sh
+++ read_options.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-lynx -dump "https://www.stw-thueringen.de/mensen/jena/mensa-ernst-abbe-platz.html" -connect_timeout=10 | tail -n +210 | head -n -332 | grep -P '^\s*\d\d\d\d-\d\d-\d\d' -B 2 -A 3
+# appropriate mensa website of your choice
+mensa_page="https://www.stw-thueringen.de/mensen/jena/mensa-ernst-abbe-platz.html"
+
+lynx --dump "$mensa_page" -connect_timeout=10 | grep -P '^\s*\d\d\d\d-\d\d-\d\d' -B 2 -A 3
